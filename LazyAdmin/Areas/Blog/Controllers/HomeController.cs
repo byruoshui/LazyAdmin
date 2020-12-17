f1573eb04e1968bf971a8890a6178cf1b7e3890c
By若水 <261658548@qq.com>
2020-12-17 11:04:17 +0800

    前端+后台架构

---
+++ LazyAdmin/Areas/Blog/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LazyAdmin.Blog.Controllers
+{
+    [Area("Blog")]
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 博客首页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
