416106d01e06db3fa0f8dd856faff41e08b112af
By若水 <261658548@qq.com>
2020-12-17 11:43:14 +0800

    修改默认路由

--- LazyAdmin/Areas/Admin/Controllers/HomeController.cs
+++ LazyAdmin/Areas/Admin/Controllers/HomeController.cs
@@ -9,7 +9,20 @@ namespace LazyAdmin.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 管理后台首页
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
+        {
+            return View();
+        }
+        
+        /// <summary>
+        /// 首页控制台
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Console()
         {
             return View();
         }
